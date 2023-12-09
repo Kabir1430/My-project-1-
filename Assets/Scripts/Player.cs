84ede7826417192e72968e2f920d8d91a14c2dc3
Kabir1430 <104447995+Kabir1430@users.noreply.github.com>
2023-12-09 19:22:10 +0530

    Box

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,15 +15,29 @@ public class Player : MonoBehaviour
     
     public float Radius;
 
-    public bool Ground;
+    public bool Ground,iscrouch;
 
     public Transform sphere;
 
-    public float smoothblend;
+    public float smoothblend,wait;
 
-    public Vector3 crouchscale,Velocity;
+    public Vector3 crouchscale,defaultscale,Velocity;
    
     public LayerMask layer;
+
+    public GameObject CardBoard,PlayerObj;
+
+    IEnumerator Crouching()
+    {
+        yield return new WaitForSeconds(wait);
+        iscrouch = true;
+    }
+    IEnumerator Crouch()
+    {
+        yield return new WaitForSeconds(wait);
+
+        iscrouch = false;
+    }
     void Update()
     {
 
@@ -51,12 +65,21 @@ public class Player : MonoBehaviour
 
 
         //Crouch
-        if(Input.GetKey(KeyCode.LeftControl))
+        if(Input.GetKey(KeyCode.LeftControl)&&!iscrouch)
         {
             transform.localScale = crouchscale;
+        CardBoard.SetActive(true);
+            PlayerObj.SetActive(false);    
+            StartCoroutine(Crouching());
+       
         }
-
-        
+        if(Input.GetKey(KeyCode.LeftControl) && iscrouch)
+        {
+            transform.localScale = defaultscale;
+            CardBoard.SetActive(false);
+            PlayerObj.SetActive(true);    
+            StartCoroutine(Crouch());   
+        }
         
         //Anim
 
